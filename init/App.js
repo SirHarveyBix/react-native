1537d008f72ee7a376dbba95a34045f8cc403864
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-06-02 15:31:46 +0200

    playin with react + native

--- init/App.js
+++ init/App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [grossStyle, setGrossStyle] = useState(true);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Good old Hello world Message !</Text>
+      <View>
+        <Text style={grossStyle && styles.gross}>
+          here's nuthin' to reveal !
+        </Text>
+      </View>
+      <Button
+        title={grossStyle ? 'Make it cleaner' : 'Make it gross'}
+        onPress={() => setGrossStyle(!grossStyle)}
+      />
     </View>
   );
 }
@@ -16,5 +26,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  gross: {
+    margin: 10,
+    borderColor: 'red',
+    borderWidth: 3,
+    padding: 8,
   },
 });
