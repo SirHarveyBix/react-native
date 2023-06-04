c4b2aca2e2da8aae17f1d32f4db8b1e0df74d200
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-06-04 16:48:15 +0200

    add image, split styles, playing whith general style

--- init/App.style.tsx
+++ init/App.style.tsx
@@ -6,30 +6,5 @@ export const styles = StyleSheet.create({
     padding: 50,
     paddingHorizontal: 16,
   },
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
-    padding: 8,
-  },
-  golasContainer: { flex: 5 },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: '#5e0acc',
-  },
-  goalText: {
-    color: 'white',
-  },
+  goalsContainer: { flex: 5 },
 });
