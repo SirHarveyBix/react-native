ccff15ace0c1f3ad3dbbc2c58e22f07468b8b7d0
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-06-06 12:35:07 +0200

    NAVIGATION: ajout de la navigation

--- 02.meal-app/screens/CategoriesScreen/index.tsx
+++ 02.meal-app/screens/CategoriesScreen/index.tsx
@@ -2,12 +2,25 @@ import { FlatList } from 'react-native';
 import { CATEGORIES } from '../../data/dummy-data';
 import { CategoryGridTile } from '../../components/CategoryGridTile';
 import Category from '../../models/category';
-
-const renderCategoryItem = ({ item }: { item: Category }) => {
-  return <CategoryGridTile title={item.title} color={item.color} />;
-};
+import { useNavigation } from '@react-navigation/native';
 
 export const CategoriesScreen = () => {
+  const navigation = useNavigation();
+
+  const renderCategoryItem = ({ item }: { item: Category }) => {
+    const pressHandler = () => {
+      navigation.navigate('MealsOverview');
+    };
+
+    return (
+      <CategoryGridTile
+        title={item.title}
+        color={item.color}
+        onPress={pressHandler}
+      />
+    );
+  };
+
   return (
     <>
       <FlatList
---
+++ 02.meal-app/screens/MealsOverviewScreen/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import { MEALS } from '../../data/dummy-data';
+import { CategoryGridTile } from '../../components/CategoryGridTile';
+import Category from '../../models/category';
+
+export const MealsOverviewScreen = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
