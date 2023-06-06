0e74050f46465c0166691d67492d90c4b5f25755
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-06-06 18:42:31 +0200

    switch drawer to bottom tab

--- 02-1-mini/App.old.drawer.tsx
+++ 02-1-mini/App.old.drawer.tsx
@@ -1,43 +1,40 @@
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import WelcomeScreen from './screens/WelcomeScreen';
 import UserScreen from './screens/UserScreen';
 import { Ionicons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 export default function App() {
-  const BottomTab = createBottomTabNavigator();
+  const Drawer = createDrawerNavigator();
 
   return (
     <NavigationContainer>
-      <BottomTab.Navigator
-        id="LeftDrawer"
-        initialRouteName="Welcome"
-        screenOptions={{
-          tabBarActiveTintColor: 'purple',
-        }}
-      >
-        <BottomTab.Screen
+      <Drawer.Navigator id="LeftDrawer" initialRouteName="Welcome">
+        <Drawer.Screen
           name="Welcome"
           component={WelcomeScreen}
           options={{
-            tabBarActiveTintColor: 'purple',
-            tabBarIcon: ({ color, size }) => (
+            drawerLabel: 'Welcome friend',
+            drawerStyle: {
+              backgroundColor: '#ccc',
+            },
+            drawerIcon: ({ color, size }) => (
               <Ionicons name="home" color={color} size={size} />
             ),
           }}
         />
-        <BottomTab.Screen
+        <Drawer.Screen
           name="User"
           component={UserScreen}
           options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="person" color={color} size={size} />
-            ),
             headerStyle: { backgroundColor: 'pink' },
             headerTintColor: 'yellow',
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+            ),
           }}
         />
-      </BottomTab.Navigator>
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
--- 02-1-mini/screens/UserScreen.tsx
+++ 02-1-mini/screens/UserScreen.tsx
@@ -4,26 +4,26 @@ import { DrawerActions } from '@react-navigation/native';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 function UserScreen() {
-  const navigation = useNavigation();
-  const isDrawerOpen = useDrawerStatus() === 'open';
+  // const navigation = useNavigation();
+  // const isDrawerOpen = useDrawerStatus() === 'open';
 
-  const openDrawerHandler = () => {
-    if (isDrawerOpen == null) {
-      navigation.dispatch(DrawerActions.openDrawer());
-    } else {
-      navigation.dispatch(DrawerActions.closeDrawer());
-    }
-  };
+  // const openDrawerHandler = () => {
+  //   if (isDrawerOpen == null) {
+  //     navigation.dispatch(DrawerActions.openDrawer());
+  //   } else {
+  //     navigation.dispatch(DrawerActions.closeDrawer());
+  //   }
+  // };
 
   return (
     <View style={styles.rootContainer}>
       <Text>
         This is the <Text style={styles.highlight}>"User"</Text> screen!
       </Text>
-      <Button
+      {/* <Button
         title={isDrawerOpen ? 'Close Drawer' : 'Open Drawer'}
         onPress={openDrawerHandler}
-      />
+      /> */}
     </View>
   );
 }
