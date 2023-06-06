c8fcb802d540f4bb4fc3335c8956baa0a789a486
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-06-06 13:29:47 +0200

    add : styling and data for mealsItem

--- 02.meal-app/components/MealItem/index.tsx
+++ 02.meal-app/components/MealItem/index.tsx
@@ -1,11 +1,34 @@
-import { Text, View } from 'react-native';
+import { Image, Pressable, Text, View } from 'react-native';
 import { styles } from './style';
 import Meal from '../../models/meal';
 
-export const MealItem = ({ title }: Partial<Meal>) => {
+export const MealItem = ({
+  title,
+  imageUrl,
+  duration,
+  complexity,
+  affordability,
+}: Meal) => {
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.mealItem}>
+      <Pressable
+        android_ripple={{ color: '#ccc' }}
+        style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+      >
+        <View style={styles.innerContainer}>
+          <View>
+            <Image source={{ uri: imageUrl }} style={styles.image} />
+            <Text style={styles.title}>{title}</Text>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.detailItem}>{duration}</Text>
+            <Text style={styles.detailItem}>{complexity?.toUpperCase()}</Text>
+            <Text style={styles.detailItem}>
+              {affordability?.toLowerCase()}
+            </Text>
+          </View>
+        </View>
+      </Pressable>
     </View>
   );
 };
--- 02.meal-app/screens/MealsOverviewScreen/index.tsx
+++ 02.meal-app/screens/MealsOverviewScreen/index.tsx
@@ -16,7 +16,7 @@ export const MealsOverviewScreen = () => {
   );
 
   const renderMealItem = ({ item }: { item: Meal }) => {
-    return <MealItem title={item.title} />;
+    return <MealItem {...item} />;
   };
   return (
     <View style={styles.container}>
