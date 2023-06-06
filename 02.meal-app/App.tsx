94a5ad249625b19dc2f92b26e50cf028ff8bbd13
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-06-06 12:35:20 +0200

    NAVIGATION: ajout de la navigation

--- 02.meal-app/App.tsx
+++ 02.meal-app/App.tsx
@@ -1,12 +1,37 @@
-import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { CategoriesScreen } from './screens/CategoriesScreen';
 import { StatusBar } from 'expo-status-bar';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { MealsOverviewScreen } from './screens/MealsOverviewScreen';
+
+export type RootStackParamList = {
+  MealsCategories: undefined;
+  MealsOverview: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          {/* first child is initial screen 
+              or : <Stack.Navigator initialRouteName="MealsCategories">
+          */}
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{ title: 'Meals Categories' }}
+          />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreen}
+            options={{ title: 'Meals Overview' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
