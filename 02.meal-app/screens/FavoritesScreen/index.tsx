b883e16b4377ea567dc59cf7fd8b576073e4a29e
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-06-07 11:09:28 +0200

    style

--- 02.meal-app/screens/FavoritesScreen/index.tsx
+++ 02.meal-app/screens/FavoritesScreen/index.tsx
@@ -2,10 +2,20 @@ import { MealList } from '../../components/MealList';
 import { useContext } from 'react';
 import { FavoriteContext } from '../../store/context/favorites.context';
 import { MEALS } from '../../data/dummy-data';
+import { Text, View } from 'react-native';
+import { styles } from './styles';
 
 export const FavoritesScreen = () => {
   const { ids } = useContext(FavoriteContext);
   const favoriteMeals = MEALS.filter((meal) => ids.includes(meal.id));
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meals yet.</Text>
+      </View>
+    );
+  }
 
   return <MealList items={favoriteMeals} />;
 };
