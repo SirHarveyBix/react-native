cf180342eea891605cb773c420f9ff257e3b9ae1
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-06-21 13:12:39 +0200

    add placeList + style

--- 05.favorite-place-app/screens/AllPlaces/index.tsx
+++ 05.favorite-place-app/screens/AllPlaces/index.tsx
@@ -1,9 +1,27 @@
+import { RouteProp, useIsFocused } from '@react-navigation/native';
 import PlacesList from '../../components/Places/PlacesList';
+import { RootStackParamList } from '../../types/types';
+import { useEffect, useState } from 'react';
+import { Place } from '../../models/place';
 
-type AllPlacesProps = {};
+type AllPlacesProps = {
+  route: RouteProp<RootStackParamList, 'AllPlaces'>;
+};
 
-const AllPlaces = ({}: AllPlacesProps) => {
-  return <PlacesList />;
+const AllPlaces = ({ route }: AllPlacesProps) => {
+  const [loadedPlaces, setLoadedPlaces] = useState<Place[]>([]);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (isFocused && route.params) {
+      setLoadedPlaces((currentPLaces) => [
+        ...currentPLaces,
+        route.params!.place,
+      ]);
+    }
+  }, [isFocused, route]);
+
+  return <PlacesList places={loadedPlaces} />;
 };
 
 export default AllPlaces;
