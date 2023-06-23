b21766d576a356998d4dab1a59e86e2eae4865ed
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-06-23 13:30:55 +0200

    add place Details

--- 05.favorite-place-app/App.tsx
+++ 05.favorite-place-app/App.tsx
@@ -69,7 +69,7 @@ export default function App() {
             name="PlaceDetails"
             component={PlaceDetails}
             options={{
-              title: 'Map',
+              title: 'Loading place .. ',
             }}
           />
         </Stack.Navigator>
--- 05.favorite-place-app/screens/PlaceDetails/index.tsx
+++ 05.favorite-place-app/screens/PlaceDetails/index.tsx
@@ -1,7 +1,64 @@
-type PlaceDetailsProps = {};
+import { ScrollView, Image, View, Text } from 'react-native';
+import OutlinedButton from '../../components/ui/OutlinedButton';
+import styles from './styles';
+import { fetchPlaceDetails } from '../../utils/utils';
+import { useEffect, useState } from 'react';
+import { RouteProp } from '@react-navigation/native';
+import { RootStackParamList } from '../../types/types';
+import { CompletePlace } from '../../models/place';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
-const PlaceDetails = ({}: PlaceDetailsProps) => {
-  return <></>;
+type PlaceDetailsProps = {
+  route: RouteProp<RootStackParamList, 'PlaceDetails'>;
+  navigation: NativeStackNavigationProp<RootStackParamList>;
+};
+
+const PlaceDetails = ({ route, navigation }: PlaceDetailsProps) => {
+  const [fetchedPlace, setFetchedPlace] = useState<CompletePlace>();
+  const selectedPlaceId = route.params?.placeId ?? '';
+
+  const showOnMapHandler = () => {
+    if (fetchedPlace) {
+      navigation.navigate('Map', {
+        initialLat: Number(fetchedPlace.location.lat),
+        initialLng: Number(fetchedPlace.location.lng),
+      });
+    }
+  };
+
+  useEffect(() => {
+    const loadPlaceDetails = async () => {
+      const place = await fetchPlaceDetails(selectedPlaceId);
+      setFetchedPlace(place);
+      navigation.setOptions({
+        title: place.title,
+      });
+    };
+
+    loadPlaceDetails();
+  }, [selectedPlaceId]);
+
+  if (fetchedPlace == null) {
+    return (
+      <View style={styles.fallback}>
+        <Text>Loadig place Data ..</Text>
+      </View>
+    );
+  }
+
+  return (
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: fetchedPlace?.imageUri }} />
+      <View style={styles.locationContainer}>
+        <View style={styles.addressContainer}>
+          <Text style={styles.address}>{fetchedPlace?.address}</Text>
+        </View>
+        <OutlinedButton icon="map" onPress={showOnMapHandler}>
+          View on Map
+        </OutlinedButton>
+      </View>
+    </ScrollView>
+  );
 };
 
 export default PlaceDetails;
